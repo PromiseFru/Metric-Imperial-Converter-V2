8747bbb511e31fc4560a3a7717bc3e038af3e00d
Promise Fru <promisefru1@gmail.com>
2020-10-20 15:25:11 +0100

    fractional input test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -29,8 +29,9 @@ suite('Unit Tests', function(){
     });
     
     test('Fractional Input', function(done) {
-      
-      //done();
+      var input = 1/2;
+      assert.equal(convertHandler.getNum(input), 0.5);
+      done();
     });
     
     test('Fractional Input w/ Decimal', function(done) {
