58903e74e4ede4cff5d8b0e02b2a2a5a75563ecb
Promise Fru <promisefru1@gmail.com>
2020-10-24 17:28:19 +0100

    invalid unit testing good

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -34,8 +34,14 @@ suite('Functional Tests', function() {
       });
       
       test('Convert 32g (invalid input unit)', function(done) {
-        
-        //done();
+        chai.request(server)
+        .get('/api/convert')
+        .query({input: '32g'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, 'invalid unit');
+          done();
+        });
       });
       
       test('Convert 3/7.2/4kg (invalid number)', function(done) {
